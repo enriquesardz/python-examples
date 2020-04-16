b1cc624583ce43a4b633ed5f4e0074e9792f48ce
Enrique <enriquesaalas@gmail.com>
2020-04-16 16:57:12 -0500

    Initial commit

---
+++ python_stacking_test.py
@@ -0,0 +1,17 @@
+user_input = int(input("Number of blocks:"))
+
+def get_height(blocks):
+    height = 0
+    total = 0
+    prev = 1
+    while(True):
+        total += prev
+        prev += 1
+        if (total <= blocks):
+            height += 1
+            print("#" * (prev-1))
+        else:
+            break
+        
+    return height
+print("Height: ", get_height(user_input))
